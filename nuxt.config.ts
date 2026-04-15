36cbcffc47dd1b76db66d0644947b346f05c5e31
Jurgo <jurgo88@gmail.com>
2026-04-15 12:22:05 +0200

    feat: update Clarity script inclusion to use src attribute for improved loading

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,8 +43,8 @@ defaultLocale: 'en',
       ],
       script: process.env.NUXT_PUBLIC_CLARITY_ID ? [
         {
-          innerHTML: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y)})(window,document,"clarity","script","${process.env.NUXT_PUBLIC_CLARITY_ID}");`,
-          type: 'text/javascript',
+          src: `https://www.clarity.ms/tag/${process.env.NUXT_PUBLIC_CLARITY_ID}`,
+          async: true,
         },
       ] : [],
     },
